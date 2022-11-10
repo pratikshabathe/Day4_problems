050c5864676ec523f2d9379d1777ad8585e0efd0
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-10 20:28:23 +0530

    create new branch

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,10 @@
 package com.bl.day4;
 
 public class SnakeLadder {
+	
+	final static byte OPTION_NO_PLAY = 0;
+	final static byte OPTION_SNAKE = 1;
+	final static byte OPTION_LADDER = 2;
 
 	public static void main(String[] args) {
 		
@@ -9,5 +13,25 @@ public class SnakeLadder {
 		
 		byte dieRollNum = (byte)((Math.random() *10) % 6 + 1);
 		System.out.println("the die show " +dieRollNum);
+		
+		byte playOption = (byte)((Math.random() * 10) % 3);
+		
+		switch (playOption) {
+		case OPTION_NO_PLAY : 
+			System.out.println("The player. not playing at " + position +"\n");
+			break;
+			
+		case OPTION_SNAKE :
+			position -= dieRollNum;
+			if(position < 0) position = 0;
+			System.out.println("stepped on snake. going back to " + position + " :\n");
+			break;
+		
+		case OPTION_LADDER : 
+			
+			position += dieRollNum;
+			System.out.println("Ladder. off we go to position "+ position + " :\n");
+			break;
+		}
 	}
 }
